0dab5a3f070ce115742eaa194403f671ceb77ac6
Segev, Ram <yrs185618@ncr.com>
2021-06-30 16:24:50 +0300

    cy examples

--- POC/chrome-extension-cypress/src/selectorPicker.ts
+++ POC/chrome-extension-cypress/src/selectorPicker.ts
@@ -22,11 +22,10 @@ export const selectorPicker = () => {
         const firstTag = parent.elem.getAttribute([selector]);
         const firstSelector = firstTag ? `[${selector}='${firstTag}']` : 'BODY';
         const cySelector = `${firstSelector} ${parent.tags.split(' ').reverse().join(' ')}`
-        console.log(cySelector);
         const innerText = document.querySelector(cySelector).textContent;
         const color = window.getComputedStyle(document.querySelector(cySelector)).color
-        console.log(`cy.get(${cySelector}).contain('have.text', '${innerText}')`);
-        console.log(`cy.get(${cySelector}).contain('have.css', 'color, '${color}')`);
+        console.log(`cy.get(${cySelector}).contains('have.text', '${innerText}')`);
+        console.log(`cy.get(${cySelector}).contains('have.css', 'color, '${color}')`);
     };
     document.addEventListener('click', useSelector, false);
     document.addEventListener('change', useSelector, false);
