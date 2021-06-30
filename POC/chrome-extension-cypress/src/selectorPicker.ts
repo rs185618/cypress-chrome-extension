92cd22aa960f6b318a5fc22a89180ab793bc543a
Segev, Ram <yrs185618@ncr.com>
2021-06-30 14:35:20 +0300

    improve selector picker

--- POC/chrome-extension-cypress/src/selectorPicker.ts
+++ POC/chrome-extension-cypress/src/selectorPicker.ts
@@ -1,15 +1,16 @@
-
 export const selectorPicker = () => {
     const getClosestParent = (elem, selector = '[id]') => {
-        let tags = '';
+        let tags = '', lastChlid = '';
+
         for (; elem && elem !== document; elem = elem.parentNode) {
-            if (elem.matches(selector)) {
+            if (elem.matches(selector) || document.body === elem) {
                 return {
                     elem: elem,
-                    tags: tags
+                    tags: tags,
                 }
             } else {
-                tags += elem.tagName + ' ';
+                let index = [].indexOf.call(elem.parentNode.children, elem) + 1;
+                tags += elem.tagName + ':nth-child('+index+') ';
             }
         }
         return null;
@@ -17,7 +18,8 @@ export const selectorPicker = () => {
     document.addEventListener('click', function (event) {
         const selector = 'data-testid';
         const parent = getClosestParent(event.target, `[${selector}]`);
-        console.log(`[${selector}='${parent.elem.getAttribute([selector])}'] ${parent.tags.split(' ').reverse().join(' ')}`);
+        const firstTag = parent.elem.getAttribute([selector]);
+        const firstSelector = firstTag ? `[${selector}='${firstTag}']` : 'BODY'
+        console.log(`${firstSelector} ${parent.tags.split(' ').reverse().join(' ')}`);
     }, false);
 }
-
