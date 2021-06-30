0fa0440ab2a5b3d6a48c95c0c6ea2c337f5cc8d5
Segev, Ram <yrs185618@ncr.com>
2021-06-30 10:40:47 +0300

    read events from tab

--- POC/chrome-extension-cypress/src/selectorPicker.ts
+++ POC/chrome-extension-cypress/src/selectorPicker.ts
@@ -12,12 +12,12 @@ export const selectorPicker = () => {
                 tags += elem.tagName + ' ';
             }
         }
+        return null;
     };
     document.addEventListener('click', function (event) {
         const selector = 'data-testid';
         const parent = getClosestParent(event.target, `[${selector}]`);
         console.log(`[${selector}='${parent.elem.getAttribute([selector])}'] ${parent.tags.split(' ').reverse().join(' ')}`);
     }, false);
-    return '';
 }
 
